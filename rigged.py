eae9403e32dbfc70f32d7db4a42ac1726fb75ed9
rottenstea <97736693+rottenstea@users.noreply.github.com>
2025-03-20 21:47:15 +0100

    Improvement suggestion rigged.py
    I added a small function that verifies that the user-provided xchance parameter of Rigged_RNG() is indeed between 0 and 1 and raises an error with message otherwise. I also added a line to test this new functionality in the bottom - this will purposefully trigger the error.

--- rigged.py
+++ rigged.py
@@ -1,6 +1,12 @@
 import numpy as np
 
-def Rigged_RNG(x, xchance, bound):
+def check_xchance(x):
+    if not (0 <= x < 1):
+        raise ValueError(f"Value for parameter xchance ({x}) is out of bounds (must be in [0, 1)).")
+
+
+
+def Rigged_RNG(x: int, xchance: float, bound:int):
 
     """
     Function that returns a random INT but is rigged to make x more likely to be returned.
@@ -9,6 +15,9 @@ def Rigged_RNG(x, xchance, bound):
     :param xchance: Likelihood that the RNG returns x, should be between 0 and 1
     :return: Random INT between 0 and bound, with higher likelihood of returning x
     """
+
+    # check that xchance is indeed between 0 and 1 first
+    check_xchance(xchance)
 
     seed = np.random.rand()
 
@@ -22,3 +31,7 @@ def Rigged_RNG(x, xchance, bound):
 
 for i in range(10):
     print(Rigged_RNG(5, 0.75, 10))
+
+# test case for the non-permitted input, will throw error
+number = Rigged_RNG(700,1.2,800)
+print(number)
